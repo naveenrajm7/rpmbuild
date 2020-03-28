3eecffe906365e37849e9df06e7b4a1fe1706487
Naveenraj M <navmuthu@cisco.com>
2020-03-29 00:58:20 +0530

    setup ts file with steps

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,36 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 async function run() {
   try {
     const myInput = core.getInput('specFile');
     core.debug(`Hello ${myInput} from inside a container`);
+
+    // Get repo files from /github/workspace/
+    await exec.exec('ls -la /github/workspace');
+
+    // LOG: know current directory
+    await exec.exec('pwd && echo $HOME && ls');
+
+    // Copy spec file from path specFile to /root/rpmbuild/SPECS/
+    
+    
+    // Get tar.gz file of release 
+
+
+    // Copy tar.gz file to /root/rpmbuild/SOURCES
+
+
+    // Execute rpmbuild 
+
+
+    // Get path for rpm 
+
+
+    // setOutput rpm_path to /root/rpmbuild/RPMS , to be consumed by other actions like 
+    // actions/upload-release-asset
+    
 
     // Get github context data
     const context = github.context;
