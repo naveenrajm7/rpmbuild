1d00b0f5e0d795466dff819010b9c53fc335eb19
Moss Cantwell <m.cantwell@gns.cri.nz>
2023-09-22 11:16:16 +1200

    fix: precheck namespacing code
    more robust check

--- el9/pre.sh
+++ el9/pre.sh
@@ -1,8 +1,6 @@
 #!/bin/bash -e
-if [ $GITHUB_ACTOR = "GeoNet" ] ; then
-printf "you are inside %s and authorised" $GITHUB_ACTOR
-exit 0
-else
-printf "you are not authorised to run as %s" $GITHUB_ACTOR
-exit 1
+if ! echo "$GITHUB_REPOSITORY" | grep -qE '^GeoNet/'; then
+  printf "you are not authorised to run under %s" $GITHUB_REPOSITORY
+  exit 1
 fi
+exit 0
